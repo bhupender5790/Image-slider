96a94b6794e074375df1fc6d4d542a76deacc830
Bhupinder <bhupenderana2000@gmail.com>
2023-03-02 16:16:48 +0530

    main

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<main>
+   <img src="https://picsum.photos/id/236/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/237/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/238/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/239/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/240/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/241/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/242/1000/500" class="slide" alt="">
+   <img src="https://picsum.photos/id/243/1000/500" class="slide" alt="">
+</main>
+<Div class="nav">
+    <button onclick="goprev()">
+        prev
+    </button >
+    <button onclick="gonext()">
+        next
+    </button>
+</Div>
+    <script src="sacript.js"></script>
+</body>
+</html>
